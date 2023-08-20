eb4f5788506f6a133550e9a78e5369fdff5645c1
Jose silva <you@example.com>
2023-08-20 08:50:45 -0300

    add animation to the buttom

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function toggleMode() {
+function toggleMode()
+{
   const html = document.documentElement
   html.classList.toggle("light")
 
@@ -8,7 +9,7 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.jpg")
   } else {
     //se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.jpg")
